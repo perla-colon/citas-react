34a40a245451512fac4d639f4221a1a3daa0a3d9
perla-colon <perla.colon.n@hotmail.com>
2022-01-04 11:55:18 -0600

    citas react

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Error({children}) {
+    return (
+        <div className='bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-sm'>
+            {children}
+        </div>
+    )
+}
+export default Error
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header({toma1Valor}){
+    return (
+        <>
+            <h1 className="font-black text-5xl text-center md:w-1/2 sm:w-1/4 mx-auto">
+                Seguimiento pacientes{' '}
+                <span className="text-indigo-600">Veterinaria</span>
+            </h1>
+        </>
+    )
+}
+export default Header;
