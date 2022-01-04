9e44c7010047e8394b32d8e55b8742fbf2fbe1d0
perla-colon <perla.colon.n@hotmail.com>
2022-01-04 12:02:48 -0600

    cambio header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header({toma1Valor}){
     return (
         <>
             <h1 className="font-black text-5xl text-center md:w-1/2 sm:w-1/4 mx-auto">
-                Seguimiento pacientes{' '}
+                Seguimiento pacientes{' '} con Github {''}
                 <span className="text-indigo-600">Veterinaria</span>
             </h1>
         </>
